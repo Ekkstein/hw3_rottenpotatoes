9298f74aeb6e2c623215752ea0fdd625a613e8d0
Edward Tadros <edward.tadros@mavencloud.com>
2013-04-22 10:50:35 +0000

    Parts 1-3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  #Movie.delete_all
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.index(e1) < page.body.index(e2)
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +28,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+        ratings = rating_list.split(", ")
+        ratings.each do |rating|
+                if uncheck then
+                        step "I uncheck \"ratings_#{rating}\""
+                else
+                        step "I check \"ratings_#{rating}\""
+                end
+        end
+end
+
+Then /^I should see (.*) movies$/ do |quantity|
+  case  quantity 
+    when "no"
+      all("tr").count == 1
+    when "all the"
+      all("tr").count == 11
+  end
 end
